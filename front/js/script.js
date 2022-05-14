9a0659204aa3188f4d382039ecc367071db4115c
Ashywar <chirigut.andrei@gmail.com>
2022-05-14 12:50:27 +0200

    added js folder

---
+++ front/js/script.js
@@ -0,0 +1,8 @@
+// Récupérer les produits sur l'API via le port 3000.
+const request = new Request('http://localhost:3000/api/products/');
+
+function get_products {
+    fetch(request)
+        .then
+
+}
